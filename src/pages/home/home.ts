082dbe249a1742495e965968c214092352ed1b50
l08084 <l08084@shibaura-it.ac.jp>
2018-03-04 23:35:50 +0900

    change

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  template: '<img #bird class="bird" src="assets/imgs/bird.png">'
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('bird') bird: ElementRef;
+  // maxY: number;
 
+  constructor(public navCtrl: NavController) {}
+  
+  ngAfterViewInit() {
+    const height = this.bird.nativeElement.offsetHeight;
+    const width = this.bird.nativeElement.offsetWidth;
+    // ViewChildで取得した要素のサイズを表示する
+    console.log(`height: ${height}`);
+    console.log(`width: ${width}`);
+  }
+
+  init = (): void => {
+    const height = this.bird.nativeElement.offsetHeight;
+    const width = this.bird.nativeElement.offsetWidth;
+    console.log(`height: ${height}`);
+    console.log(`width: ${width}`);
   }
 
 }
